7c68366deaf67f74f3ef0c9c13e801ba7d05f69d
xusenali <github6121@gmail.com>
2025-03-30 13:32:37 +0300

    yangi uzgarishlar

---
+++ js/product.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        id: 1,
+        name: "Колбаса КДС-1218",
+        price: 599.99,
+        image: "https://dostavo4ka.uz/upload-file/2021/05/05/3794/750x750-00b7fc1c-30a8-4fd8-abac-3506c7ac2c90.jpg",
+        category: "Мясные изделия"
+    },
+    {
+        id: 2,
+        name: "Колбаса нарезка МДОА",
+        price: 44.50,
+        image: "https://img.fix-price.com/_marketplace/images/origin/88/889317c5e79c5fd83883dffdb4d463f5.jpg",
+        category: "Мясные изделия"
+    },
+    {
+        id: 3,
+        name: "Колбаса КДС-1218",
+        price: 159.99,
+        image: "https://www.ermolino-produkty.ru/picts/products/kolbasa-syrokopchenaya-burgundskaya_2.jpg",
+        category: "Мясные изделия"
+    },
+    {
+        id: 4,
+        name: "Молоко 1л",
+        price: 49.39,
+        image: "https://yukber.uz/image/cache/catalog/Screenshot_32-700x700.jpg",
+        category: "Молочные продукты"
+    },
+    {
+        id: 5,
+        name: "https://dostavo4ka.uz/upload-file/2021/05/05/2097/750x750-96f203fa-9b93-4ad3-ae2b-c8d015830013.jpg",
+        price: 77.99,
+        image: "images/kolbasa4.png",
+        category: "Мясные изделия"
+    },
+    {
+        id: 6,
+        name: "https://www.m2-shop.ru/img/products_photos/120576906.jpg",
+        price: 599.99,
+        image: "images/kolbasa5.png",
+        category: "Мясные изделия"
+    }
+];
+
+console.log(products);
